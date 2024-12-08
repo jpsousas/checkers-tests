a6ae8de67c00c4ba5aebc33f248d34bd9ebcaffb
jpsousas <joao.sousa@acad.ufsm.br>
2024-12-08 18:56:36 -0300

    adicao de arqs __init__ e criacao de testes pra ai.py

--- python_checkers/tests/test_ai.py
+++ python_checkers/tests/test_ai.py
@@ -4,4 +4,10 @@ from python_checkers.ai import AI
 from python_checkers.board import Board
 
 def test_AI_init():
-    
+    black='B'
+    white='W'
+
+    first_ai_instance = AI(black)
+    second_ai_instance = AI(white)
+    assert first_ai_instance.color == black
+    assert second_ai_instance.color == white
